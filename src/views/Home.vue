9a32adf7ea83294fda56e7ea6b7a5b565c78e133
pa3datka <63630679+pa3datka@users.noreply.github.com>
2020-11-08 20:45:09 +0300

    modified Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,55 @@
   <v-container>
     <h1>Home</h1>
     <h1>{{user}}</h1>
-    <v-item-group>
+    <v-item-group
+      v-model="selected"
+      multiple
+    >
+      <v-row>
+        <v-col
+          v-for="(item, i) in this.tests"
+          :key="i"
+          cols="12"
+          md="6"
+        >
+         <v-card>
+           <v-card-title>
+             {{ item.title}}
+           </v-card-title>
+           <v-card-text>
 
+           </v-card-text>
+         </v-card>
+        </v-col>
+      </v-row>
     </v-item-group>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Home',
+  data () {
+    return {
+      selected: [],
+      tests: []
+    }
+  },
   components: {
   },
   computed: {
     user () {
       return this.$store.getters.GET_USER
     }
+  },
+  created () {
+    axios({ url: 'http://127.0.0.1:8000/api/test', method: 'GET' })
+      .then(response => {
+        console.log(response)
+        this.tests = response.data
+      })
   }
 }
 </script>
